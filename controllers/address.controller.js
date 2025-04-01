22c75613088009006884a9a2f7885ece870ae64b
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2025-04-01 23:58:19 +0100

    Restructuring

--- controllers/address.controller.js
+++ controllers/address.controller.js
@@ -9,6 +9,37 @@ const getAll =  async (req, res) => {
     }
   }
 
+const getOne =  async (req, res) => {
+    try {
+      const address = await Address.findById(req.params.id);
+      if (!address) return res.status(404).send({ message: "Address not found" });
+      res.status(200).send(address);
+    } catch (error) {
+      return res.status(500).send({ message: error.message });
+    }
+  }
+
+const create = async (req, res) => {
+  const { error } = addressValidator(req.body);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+  const { cityName, description, isHeadquarter } = req.body;
+  try {
+    const newAddress = new Address({
+      cityName,
+      description,
+      isHeadquarter,
+    });
+    const savedAddress = await newAddress.save();
+    res.status(201).send(savedAddress);
+  } catch (error) {
+    res.status(500).send({ message: error.message });
+  }
+}
+
+
+
   module.exports = {
-    getAll
+    getAll,
+    create,
+    getOne,
   }
--- routes/address.js
+++ routes/address.js
@@ -2,39 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { Address, addressValidator } = require("../models/address");
 const { auth } = require("../middleware/auth");
-const { getAll } = require("../controllers/address.controller");
+const { getAll, getOne, create } = require("../controllers/address.controller");
 
+//, auth
 //Getting list of address
-router.get("/", auth, getAll);
+router.get("/", getAll);
 
 //Creating new Address
-router.post("/", auth, async (req, res) => {
-  const { error } = addressValidator(req.body);
-  if (error) return res.status(400).send({ message: error.details[0].message });
-  const { cityName, description, isHeadquarter } = req.body;
-  try {
-    const newAddress = new Address({
-      cityName,
-      description,
-      isHeadquarter,
-    });
-    const savedAddress = await newAddress.save();
-    res.status(201).send(savedAddress);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
+router.post("/", create);
 
 //Get a single address
-router.get("/:id", auth, async (req, res) => {
-  try {
-    const address = await Address.findById(req.params.id);
-    if (!address) return res.status(404).send({ message: "Address not found" });
-    res.status(200).send(address);
-  } catch (error) {
-    return res.status(500).send({ message: error.message });
-  }
-});
+router.get("/:id",getOne);
 
 //Editing address
 router.put("/:id", auth, async (req, res) => {
