755aa737cf4579fa8a0075780595f1f623da8b5b
Abdulsalam Akinyoola <abdulsalamakinyoola@gmail.com>
2025-04-04 00:55:54 +0100

    code refactor user controller and route fixed

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,8 +1,14 @@
 const express = require("express")
+const indexRoutes = express.Router()
 const addressRoutes = require("../routes/address")
 const productRoutes = require("../routes/products")
-const indexRoutes = express.Router()
+const userroutes = require("../routes/users")
+
+
 
 indexRoutes.use("/address",addressRoutes)
 indexRoutes.use("/product", productRoutes)
+indexRoutes.use("/", userroutes)
+
+
 module.exports = indexRoutes
